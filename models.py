7665b8eee6f912c02e7bbf8cef21f60c5d32749e
Timo <timo.land@gmx.de>
2020-05-20 11:11:04 +0200

    Cleanup
    - MultiLayerPerceptron replaces oneHidden_reluAct_net & oneHidden_linear_net
    -> can be deleted
    - learn.py is outdated

--- models.py
+++ models.py
@@ -1,21 +1,11 @@
-import linear_classifier
-import oneHidden_reluAct_net
 import multilayerperceptron
 
 
 #                   #
 #       models      #
 #                   #
-def create_model(model_name, dataset, nHiddenLayers=0):
+def create_model(model_name, dataset, nHiddenLayers=0, loss_func=None):
     switcher={
-        'linear':linear_classifier.LinearNetwork(dataset.full_data.shape[1], dataset.label_encoder.classes_,
-                                name= model_name,
-                                mean=dataset.scaler.mean_, variance=dataset.scaler.var_,
-                                isInputAbsolute=not dataset.is_relative),
-        'simple_nn':oneHidden_reluAct_net.OneHiddenNetwork(dataset.full_data.shape[1], dataset.label_encoder.classes_,
-                                name= model_name,
-                                mean=dataset.scaler.mean_, variance=dataset.scaler.var_,
-                                isInputAbsolute=not dataset.is_relative),
         'multi_layer_perceptron':multilayerperceptron.MultiLayerPerceptron(dataset.full_data.shape[1], dataset.label_encoder.classes_,
                                 name= model_name,
                                 mean=dataset.scaler.mean_, variance=dataset.scaler.var_,
