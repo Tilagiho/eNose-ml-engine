b7837861e10ef490d30098fd1e53ec5edae911f1
Timo <timo.land@gmx.de>
2020-06-03 20:07:56 +0200

    Refactor
    - MultiLabelClassificationInterpretation does not subcluss fastai Interpretation anymore
    - added support for scikit SVC support vector machines
    MultiLabelClassificationInterpretation: svm support fix
    
    
    fix
    
    
    fix 2
    
    
    fix 3
    
    
    fix 4
    
    
    fix 6
    
    
    fix 7
    
    
    fix 8
    
    
    fix 8
    
    
    fix 9
    
    
    fix 10
    
    
    fix 11

--- fastai_additions.py
+++ fastai_additions.py
@@ -4,32 +4,48 @@ from fastai.callbacks import *
 from fastai.basic_data import *
 from fastai.basic_train import *
 
-from sklearn.metrics import multilabel_confusion_matrix
+from sklearn.svm import SVC
 
-from fastai.train import *
+from funcdataset import *
 
 import pandas
 
-class MultiLabelClassificationInterpretation(Interpretation):
+#               #
+class MultiLabelClassificationInterpretation:
     "Interpretation methods fpr multi-label classification models."
-    def __init__(self, learn:Learner, preds:Tensor, y_true:Tensor, losses:Tensor, ds_type:DatasetType=DatasetType.Valid):
-        super().__init__(learn, preds, y_true, losses, ds_type)
+    def __init__(self, dataset:FuncDataset, learn:Learner=None, svm:SVC=None, thresh:float=0.3):
+        # if interpretation of fastai Learner
+        if learn is not None:
+            preds, y, losses = learn.get_preds(with_loss=True)
 
-        # # prepare predicted labels
-        # apply threshold to predictions
-        pred_classes = torch.zeros_like(preds, dtype=torch.bool)
-        pred_classes[preds>0.3] = True
+            # # prepare predicted labels
+            # apply threshold to predictions
+            pred_classes = torch.zeros_like(preds, dtype=torch.bool)
+            pred_classes[preds>thresh] = True
 
-        # add column for no class being predicted
-        no_class_predicted = ~pred_classes.any(dim=1)
-        no_class_predicted.unsqueeze_(1)
-        self.pred_classes = torch.cat([pred_classes, no_class_predicted], dim=1)
+            # add column for no class being predicted
+            no_class_predicted = ~pred_classes.any(dim=1)
+            no_class_predicted.unsqueeze_(1)
+            pred_classes = torch.cat([pred_classes, no_class_predicted], dim=1)
 
-        # # prepare true classes
-        y_true = self.y_true.bool()
-        no_class_true = ~y_true.any(dim=1)
-        no_class_true.unsqueeze_(1)
-        self.true_classes = torch.cat([y_true, no_class_true], dim=1)
+            # # prepare true classes
+            y_true = y.bool()
+            no_class_true = ~y_true.any(dim=1)
+            no_class_true.unsqueeze_(1)
+            y_true = torch.cat([y_true, no_class_true], dim=1)
+
+        # if interpretation of support vector machine
+        elif svm is not None:
+            y_true = torch.zeros([dataset.test_classes.shape[0], dataset.c]).long()
+            y_true.scatter_(1, dataset.test_classes.long().unsqueeze(dim=1), 1.)
+            preds = torch.from_numpy(svm.predict_proba(dataset.test_set))
+            pred_classes = torch.zeros_like(preds, dtype=torch.bool)
+            pred_classes[preds>thresh] = True
+            losses = None   # TODO
+        else:
+            raise ValueError("Either learn or svm has to be specified!")
+
+        self.learn, self.y_true, self.losses, self.preds, self.pred_classses, self.dataset = learn, y_true, losses, preds, pred_classes, dataset
 
     def confusion_matrix(self, slice_size: int = None, target_type='single_class_labels'):
         "Confusion matrix as an `np.ndarray`. Only implemented for targets with exactly one label."
@@ -37,16 +53,16 @@ class MultiLabelClassificationInterpretation(Interpretation):
             # TODO: use sci-kit multilabel conf matrix
             raise NotImplementedError
 
-        cm = torch.zeros([self.true_classes.shape[1], self.true_classes.shape[1]])
+        cm = torch.zeros([self.y_true.shape[1], self.y_true.shape[1]])
 
         if slice_size is None:
-            for i in range(self.true_classes.shape[0]):
-                c = self.true_classes[i].long().argmax(dim=0)
+            for i in range(self.y_true.shape[0]):
+                c = self.y_true[i].long().argmax(dim=0)
 
-                for j in range(self.true_classes.shape[1]):
-                    if i == c and self.pred_classes[c, c]:
+                for j in range(self.y_true.shape[1]):
+                    if i == c and self.pred_classses[c, c]:
                         cm[c, c] += 1
-                    elif self.pred_classes[i, j]:
+                    elif self.pred_classses[i, j]:
                         cm[c, j] += 1
         else:
             raise NotImplementedError
@@ -63,9 +79,9 @@ class MultiLabelClassificationInterpretation(Interpretation):
         fig = plt.figure(**kwargs)
         plt.imshow(cm, interpolation='nearest', cmap=cmap)
         plt.title(title)
-        tick_marks = np.arange(self.data.c + 1)     # c + 1, because multilabeled datasets don't include 'No Smell'
-        plt.xticks(tick_marks, self.data.y.classes, rotation=90)
-        plt.yticks(tick_marks, self.data.y.classes, rotation=0)
+        tick_marks = np.arange(self.dataset.c + 1)     # c + 1, because multilabeled datasets don't include 'No Smell'
+        plt.xticks(tick_marks, self.dataset.y.classes, rotation=90)
+        plt.yticks(tick_marks, self.dataset.y.classes, rotation=0)
 
         if plot_txt:
             thresh = cm.max() / 2.
@@ -75,7 +91,7 @@ class MultiLabelClassificationInterpretation(Interpretation):
                          color="white" if cm[i, j] > thresh else "black")
 
         ax = fig.gca()
-        ax.set_ylim(len(self.data.y.classes) + 1 - .5, -.5) # len + 1, because multilabeled datasets don't include 'No Smell'
+        ax.set_ylim(len(self.dataset.y.classes) + 1 - .5, -.5) # len + 1, because multilabeled datasets don't include 'No Smell'
 
         plt.tight_layout()
         plt.ylabel('Actual')
